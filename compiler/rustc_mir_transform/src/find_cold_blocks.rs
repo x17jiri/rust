4ed468f42a236761dfc75af49387d72d716ed86c
Jiri Bobek <jiri.bobek@gmail.com>
2024-01-25 00:44:19 +0100

    correction of find_cold_blocks pass

--- compiler/rustc_mir_transform/src/find_cold_blocks.rs
+++ compiler/rustc_mir_transform/src/find_cold_blocks.rs
@@ -1,5 +1,6 @@
 //! Finds cold basic blocks
 
+use rustc_index::IndexVec;
 use rustc_middle::middle::codegen_fn_attrs::CodegenFnAttrFlags;
 use rustc_middle::mir::*;
 use rustc_middle::ty::{self, TyCtxt};
@@ -7,15 +8,19 @@ use smallvec::SmallVec;
 
 pub struct FindColdBlocks;
 
+struct Edge {
+    from: BasicBlock,
+    to: BasicBlock,
+}
+
 impl<'tcx> MirPass<'tcx> for FindColdBlocks {
     fn run_pass(&self, tcx: TyCtxt<'tcx>, body: &mut Body<'tcx>) {
         let local_decls = &body.local_decls;
 
         // Find initial set of cold blocks
-        let mut unprocessed = SmallVec::<[BasicBlock; 5]>::new();
+        let mut new = SmallVec::<[BasicBlock; 5]>::new();
 
         let bbs = body.basic_blocks.as_mut();
-
         for i in bbs.indices() {
             let terminator = bbs[i].terminator.as_mut().unwrap();
             if let TerminatorKind::Call { func, .. } = &mut terminator.kind
@@ -23,32 +28,62 @@ impl<'tcx> MirPass<'tcx> for FindColdBlocks {
             {
                 let attrs = tcx.codegen_fn_attrs(def_id);
                 if attrs.flags.contains(CodegenFnAttrFlags::COLD) {
-                    bbs[i].is_cold = true;
-                    unprocessed.push(i);
+                    new.push(i);
                 }
             }
         }
 
-        // Temporary vector to store the successors of a basic block
-        // I haven't found a way around the borrow checker without this
-        let mut succ = SmallVec::<[BasicBlock; 5]>::new();
+        //eprintln!("FindColdBlocks");
+
+        if new.is_empty() {
+            return;
+        }
+
+        // The preparation of predecessors is done in order to avoid quadratic complexity
+
+        // Find all edges
+        let mut edges = Vec::new();
+        for i in bbs.indices() {
+            let terminator = bbs[i].terminator.as_ref().unwrap();
+            for j in terminator.successors() {
+                edges.push(Edge { from: i, to: j });
+            }
+        }
+
+        // a slice with predecessors for each basic block
+        let mut pred = IndexVec::<BasicBlock, &[Edge]>::from_elem_n(&[], bbs.len());
+
+        // sort edges by target
+        edges.sort_by_key(|e| e.to);
+        let mut edges = edges.as_slice();
+        // split by target
+        while !edges.is_empty() {
+            let head = edges.split(|e| e.to != edges[0].to).next().unwrap();
+            edges = &edges[head.len()..];
+
+            pred[head[0].to] = head;
+        }
+        let pred = &pred;
 
         // update until fixpoint
-        // worst case complexity:
-        //     every block will be visited at most N times,
-        //     where N is number of successors
-        while !unprocessed.is_empty() {
-            let i = unprocessed.pop().unwrap();
+        while !new.is_empty() {
+            //eprintln!("processing cold block");
+
+            let i = new.pop().unwrap();
             if bbs[i].is_cold {
                 continue;
             }
+            bbs[i].is_cold = true;
 
-            succ.clear();
-            succ.extend(bbs[i].terminator.as_ref().unwrap().successors());
+            // A new block was just marked cold.
+            // Visit all its predecessors and if a predecessor has only cold successors,
+            // add the predecessor to the list, so it will be also marked cold.
 
-            if succ.len() > 0 && succ.iter().all(|bb| bbs[*bb].is_cold) {
-                bbs[i].is_cold = true;
-                unprocessed.push(i);
+            for &Edge { from, .. } in pred[i] {
+                let mut succ = bbs[from].terminator.as_ref().unwrap().successors();
+                if succ.all(|succ| bbs[succ].is_cold) {
+                    new.push(from);
+                }
             }
         }
     }
